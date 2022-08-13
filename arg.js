2fa67657168ee13d4c676dc9bad5377ace7eb388
liujinge <liujinge.work@gmail.com>
2022-08-13 15:51:35 +0800

    feat: 获取命令行参数

---
+++ arg.js
@@ -0,0 +1,23 @@
+/**
+ * 获取命令行参数
+ */
+// 获取所有参数
+process.argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`)
+})
+
+// 排除前两项后的参数
+const args = process.argv.slice(2);
+
+// node arg.js --name=joe
+console.log(args) // ['name=joe']
+
+console.log(args[0]) // name=ge
+
+/**
+ * 获取解析后的参数
+ */
+const parseArgs = require('minimist')(process.argv.slice(2))
+
+// node arg.js --name=joe
+console.log(parseArgs.name) // joe
